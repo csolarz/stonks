065ca6e15ecab77b035885eeec094bbabbbd476d
Charly <carlossolarz@gmail.com>
2025-10-17 15:05:44 -0300

    rename props.

--- domain/observable.go
+++ domain/observable.go
@@ -2,12 +2,12 @@ package domain
 
 // Updater representa al observador que reacciona a los cambios del sujeto
 type Updater interface {
-	Update(s *Stock)
+	Update(a *Stock)
 }
 
 // Subject representa al sujeto que notifica a sus observadores
 type Subject interface {
-	Subscribe(u Updater)
-	Unsubscribe(u Updater)
+	Subscribe(o Updater)
+	Unsubscribe(o Updater)
 	Notify()
 }
